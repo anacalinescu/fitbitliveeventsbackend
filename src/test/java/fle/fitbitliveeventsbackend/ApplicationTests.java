82c21801fe9e01b49e503e400a78a5a0fc7602ab
alin-f-tache <alin.f.tache@gmail.com>
2020-03-10 10:08:19 +0200

    First commit

---
+++ src/test/java/fle/fitbitliveeventsbackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fle.fitbitliveeventsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
